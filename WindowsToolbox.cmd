7b1c2e09670d6fe29b6edd2db075f2312c9f70f8
beerpsi <92439990+beerpiss@users.noreply.github.com>
2022-04-21 21:23:02 +0700

    feat: allow launching with .cmd outside code directory

--- WindowsToolbox.cmd
+++ WindowsToolbox.cmd
@@ -1,2 +1,4 @@
-powershell "& ""./main.ps1"""
+:: https://stackoverflow.com/questions/3827567/how-to-get-the-path-of-the-batch-script-in-windows
+SET scriptdir=%~dp0
+powershell "& ""%scriptdir:~0,-1%\main.ps1"""
 exit
